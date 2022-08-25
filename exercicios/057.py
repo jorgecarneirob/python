1346a03c9188650e173f892f7dbfaa3d581211ee
jorgecarneirob <50383738+jorgecarneirob@users.noreply.github.com>
2022-08-25 17:02:54 -0300

    correcao ex 57 e 58

--- exercicios/057.py
+++ exercicios/057.py
@@ -1,9 +1,7 @@
 print('\033[31m* \033[m' * 50)
-print('''Este código usa while
-Para verificar o sexo.'''.center(100))
+print('''Este código usa while Para verificar o sexo.'''.center(100))
 print('\033[31m* \033[m' * 50)
-#r = ''
-sexo = input('Digite [M/F]')
-while sexo in != 'MmFf':
-    print('\033[31m[ \033[mERRO\033[31m ]\033[m Digite uma valor válido.')
-print(sexo)
+sexo = input('Digite [M/F]').strip().upper()[0]
+while sexo not in 'MmFf':
+    sexo = input('\033[31m[ \033[mERRO\033[31m ]\033[m Digite uma valor válido.\n Digite [M/F]').strip().upper()[0]
+print('Sexo {} registadro'.format(sexo))
--- exercicios/058.py
+++ exercicios/058.py
@@ -2,21 +2,20 @@ from random import randint
 print('\033[31m* \033[m' * 50)
 print('''Este código faz um sorteio.'''.center(100))
 print('\033[31m* \033[m' * 50)
-valido = 0
+valido = False
 tentativas = 0
-while valido == 0:
+while not valido:
     pc = randint(0,3)
     user = int(input('Digite um número entre 0 - 3: '))
     if pc == user:
         print('''O valor sorteado foi {}.
 você digitou {}
-Você tentou {} para acertar!
+Você tentou {}x para acertar!
 '''.format(pc, user, tentativas))
-        valido = 1
+        valido = True
     else:
         print('''O valor sorteado foi {}.
 você digitou {}
 TENTE NOVAMENTE.
 =================================='''.format(pc, user))
         tentativas +=1
-        valido = 0
