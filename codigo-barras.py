b033ff20a8e9e79a89a47c9b247815ae7e289a37
jorgecarneirob <50383738+jorgecarneirob@users.noreply.github.com>
2022-08-28 21:53:31 -0300

    limpeza repo

--- codigo-barras.py
+++
@@ -1,8 +0,0 @@
-#autor: Jorge Carneiro
-#email:jorgecarneirob@hotmail.com
-
-from barcode import EAN13
-from barcode.writer import ImageWriter
-codigo = input('Informe o EAN: ')
-with open(r'C:\Users\Fer Lima\Documents\GitHub\python\codbarras.png','wb') as f:
-    EAN13(codigo, writer=ImageWriter()).write(f)
